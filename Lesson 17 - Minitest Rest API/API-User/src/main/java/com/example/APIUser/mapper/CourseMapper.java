0f9b1382328f157b1d6c7cb22a74cfaf54b50aed
Ringo7996 <linhnt.jpc@gmail.com>
2023-04-22 12:27:48 +0700

    throw exception

--- Lesson 17 - Minitest Rest API/API-User/src/main/java/com/example/APIUser/mapper/CourseMapper.java
+++ Lesson 17 - Minitest Rest API/API-User/src/main/java/com/example/APIUser/mapper/CourseMapper.java
@@ -3,18 +3,25 @@ package com.example.APIUser.mapper;
 import com.example.APIUser.dto.CourseRecord;
 import com.example.APIUser.entity.Course;
 import com.example.APIUser.entity.User;
+import com.example.APIUser.exception.NotFoundException;
 
 import java.util.List;
 
 public class CourseMapper {
     public static CourseRecord toCourseRecord(Course course, List<User> users) {
-        User user = new User();
+        User user = null;
         for (User u : users) {
             if (u.getId() == course.getUserId()) {
                 user = u;
                 break;
             }
         }
+
+
+        if (user == null) {
+            throw new NotFoundException("User" + course.getUserId() + "not found");
+        }
+
         return new CourseRecord(course.getId(), course.getName(), course.getDescription(), course.getType(), course.getTopics(), course.getThumbnail(), user);
     }
 }
